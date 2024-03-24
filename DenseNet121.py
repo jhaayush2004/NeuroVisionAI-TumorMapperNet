0f325868f8efd67b357f27e04268a1ee374e93e8
Ayush Shaurya Jha <164651751+jhaayush2004@users.noreply.github.com>
2024-03-24 11:35:51 +0530

    Update DenseNet121.py

--- DenseNet121.py
+++ DenseNet121.py
@@ -1,3 +1,6 @@
 from tensorflow.keras.applications import DenseNet121
 clf_model = DenseNet121(weights='imagenet', include_top=False, input_tensor=Input(shape=(256,256,3)))
 clf_model.summary()
+# before this i tried with trainable layer but the accuracy was less as compared
+for layer in clf_model.layers:
+    layers.trainable = False
